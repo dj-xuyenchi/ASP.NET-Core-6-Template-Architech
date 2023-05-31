33e925ae7ea844df90b270f0803202d466d23ac2
Quang Anh <89133721+dj-xuyenchi@users.noreply.github.com>
2023-06-01 00:33:47 +0700

    Add project files.

---
+++ CMS_WEB/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CMS_WEB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            //Accept CORS
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://localhost:8080",
+                                                          "http://www.contoso.com").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                                  });
+            });
+
+
+            //Fix JSON circle
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
+
+            //Json Web Token 
+            var secretKeyBytes = Encoding.UTF8.GetBytes(Settings.SecretKey());
+            // Tạo xác thực
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    //Tu Cap Token
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+
+                    //Ky vao Token
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretKeyBytes),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            // Tạo phân quyền cho API
+            builder.Services.AddAuthorization(option =>
+            {
+                option.AddPolicy("ADMINANDMEMBER", policy =>
+                    policy.RequireClaim(
+                         ClaimTypes.Role, new string[] { "ADMIN", "MEMBER" }
+                        )
+                );
+                option.AddPolicy("MEMBER", policy => policy.RequireClaim(
+                    ClaimTypes.Role, "MEMBER"
+                    ));
+                option.AddPolicy("ADMIN", policy => policy.RequireClaim(
+                 ClaimTypes.Role, "ADMIN"
+                 ));
+            });
+
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseCors(MyAllowSpecificOrigins);
+            }
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ CMS_WebDesignCore/AuthModel/AuthDataRespon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dj_webdesigncore.AuthModel
+{
+    public class AuthDataRespon
+    {
+        public int id { get; set; }
+        public byte[] avatar { get; set; }
+        public TokenModel Token { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string nickName { get; set; }
+        public int role { get; set; }
+    }
+}
